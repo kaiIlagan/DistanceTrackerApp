da57a3d736a7c917c482be3c55441061f66eb16e
Kayin Ilagan <kayinilagan@gmail.com>
2022-07-17 13:58:14 -0400

    Start Location Updates, currently not working with log

--- app/src/main/java/com/example/distancetrackerapp/service/TrackerService.kt
+++ app/src/main/java/com/example/distancetrackerapp/service/TrackerService.kt
@@ -5,14 +5,20 @@ import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
 import android.content.Intent
 import android.os.Build
+import android.os.Looper
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
 import com.example.distancetrackerapp.util.Constants.ACTION_SERVICE_START
 import com.example.distancetrackerapp.util.Constants.ACTION_SERVICE_STOP
+import com.example.distancetrackerapp.util.Constants.LOCATION_FASTEST_UPDATE_INTERVAL
+import com.example.distancetrackerapp.util.Constants.LOCATION_UPDATE_INTERVAL
 import com.example.distancetrackerapp.util.Constants.NOTIFICATION_CHANNEL_ID
 import com.example.distancetrackerapp.util.Constants.NOTIFICATION_CHANNEL_NAME
 import com.example.distancetrackerapp.util.Constants.NOTIFICATION_ID
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -25,8 +31,22 @@ class TrackerService: LifecycleService() {
     @Inject
     lateinit var notificationManager: NotificationManager
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     companion object {
         val started = MutableLiveData<Boolean>()
+    }
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            result.locations.let{ locations->
+                for(location in locations){
+                    val newLatLng = LatLng(location.latitude, location.longitude)
+                    Log.d("TrackerService", newLatLng.toString())
+                }
+            }
+        }
     }
 
     private fun setInitialValues(){
@@ -35,6 +55,7 @@ class TrackerService: LifecycleService() {
 
     override fun onCreate() {
         setInitialValues()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         super.onCreate()
     }
 
@@ -44,6 +65,7 @@ class TrackerService: LifecycleService() {
                 ACTION_SERVICE_START -> {
                     started.postValue(true)
                     startForegroundService()
+                    startLocationUpdates()
                 }
                 ACTION_SERVICE_STOP -> {
                     started.postValue(false)
@@ -58,8 +80,22 @@ class TrackerService: LifecycleService() {
         createNotificationChannel()
         startForeground(NOTIFICATION_ID, notification.build())
     }
+
     // Remove Location ?s
     // replace with LocationRequest.create().apply see Dependency Video 1
+    private fun startLocationUpdates(){
+        val locationRequest = LocationRequest.create().apply {
+            interval = LOCATION_UPDATE_INTERVAL
+            fastestInterval = LOCATION_FASTEST_UPDATE_INTERVAL
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
     private fun createNotificationChannel(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             val channel = NotificationChannel(
